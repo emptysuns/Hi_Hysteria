22eb55e19d65d1973f595cb6d5b4fab48bad68c6
emptysuns <74051228+emptysuns@users.noreply.github.com>
2021-09-19 19:29:25 +0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,3 +1,5 @@
+#!/bin/sh
+#Author:https://github.com/emptysuns
 echo "\033[35m******************************************************************\033[0m"
 echo " ██      ██                    ██                  ██          
 ░██     ░██  ██   ██          ░██                 ░░           
@@ -12,11 +14,14 @@ echo "\033[32mGithub:\033[0m https://github.com/emptysuns/HiHysteria"
 echo "\033[35m******************************************************************\033[0m"
 echo "\033[41;37mReady to install!\033[0m\n\n"
 echo  "\033[42;37mDowload:hysteria主程序... \033[0m"
+wget -O /etc/hysteria/hysteria https://github.com/HyNetwork/hysteria/releases/download/v0.8.5/hysteria-linux-amd64
+chmod 755 /etc/hysteria/hysteria
+wget -O /etc/hysteria/hysteria/chnroutes.acl https://raw.githubusercontent.com/emptysuns/HiHysteria/main/acl/chnroutes.acl
 echo "\033[32m下载完成！\033[0m"
 echo  "\033[42;37m开始配置: \033[0m"
 echo "\033[32m请输入您的域名(必须是存在的域名，并且解析到此ip):\033[0m"
 read  domain
-echo "\033[32m请输入你想要开启的端口（此端口是server的开启端口）：\033[0m"
+echo "\033[32m请输入你想要开启的端口（此端口是server的开启端口10000-65535）：\033[0m"
 read  port
 echo "期望速度，请如实填写，这是客户端的峰值速度，服务端默认不受限。\033[31m期望过低或者过高会影响转发速度！\033[0m"
 echo "\033[32m请输入客户端期望的下行速度:\033[0m"
@@ -25,8 +30,8 @@ echo "\033[32m请输入客户端期望的上行速度:\033[0m"
 read  upload
 echo "\033[32m请输入混淆口令（相当于连接密钥）:\033[0m"
 read  obfs
-echo "\033[32m配置完成！\033[0m"
-echo  "\033[42;37m执行配置：\033[0m"
+echo "\033[32m配置录入完成！\033[0m"
+echo  "\033[42;37m执行配置...\033[0m"
 cat <<EOF > /etc/hysteria/config.json
 {
   "listen": ":$port",
@@ -44,6 +49,7 @@ cat <<EOF > /etc/hysteria/config.json
       "password": "pekopeko"
     }
   },
+  "acl": "/etc/hysteria/hysteria/chnroutes.acl",
   "recv_window_conn": 15728640,
   "recv_window_client": 67108864,
   "max_conn_client": 4096,
@@ -61,6 +67,7 @@ cat <<EOF > config.json
 "timeout" : 300,
 "disable_udp": false
 },
+"acl": "chnroutes.acl",
 "obfs": "$obfs",
 "auth_str": "pekopeko",
 "server_name": "$domain",
@@ -69,8 +76,6 @@ cat <<EOF > config.json
 "recv_window": 67108864,
 "disable_mtu_discovery": false
 }
-
-
 EOF
 
 cat <<EOF >/etc/systemd/system/hysteria.service
@@ -91,7 +96,6 @@ chmod 644 /etc/systemd/system/hysteria.service
 systemctl daemon-reload
 systemctl enable hysteria
 systemctl start hysteria
-echo "Tips:客户端默认只开启http代理!http://127.0.0.1:8888"
-echo  "\033[42;37m所有安装已经完成，配置文件已经在本目录生成！\033[0m"
+echo "Tips:客户端默认只开启http代理!http://127.0.0.1:8888\n\n"
+echo  "\033[42;37m所有安装已经完成，配置文件已经在本目录生成！\033[0m\n\n"
 cat ./config.json
-
