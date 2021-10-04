1bde7ae8c2690128c889432bebc6196f6f88ff11
emptysuns <74051228+emptysuns@users.noreply.github.com>
2021-10-04 19:12:10 +0800

    Add files via upload

---
+++ reinstall.sh
@@ -0,0 +1,4 @@
+#!/bin/sh
+sh <(curl -fsSL https://raw.githubusercontent.com/emptysuns/Hi_Hysteria/main/uninstall.sh)
+sh <(curl -fsSL https://raw.githubusercontent.com/emptysuns/Hi_Hysteria/main/install.sh)
+
