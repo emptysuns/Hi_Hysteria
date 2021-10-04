9f4c645f54485f511490fac50ec110269e1d597e
emptysuns <74051228+emptysuns@users.noreply.github.com>
2021-10-04 19:54:29 +0800

    Delete uninstall.sh

--- uninstall.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-systemctl stop hysteria
-systemctl disable hysteria
-rm -rf /etc/systemd/system/hysteria.service
-systemctl daemon-reload
-rm -rf /etc/hysteria/
-rm -rf /root/config.json
-echo "Uninstall complete!"
