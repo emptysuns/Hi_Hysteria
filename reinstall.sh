dcd93594564722e0d361b5043dbfbc80c1f0823b
emptysuns <74051228+emptysuns@users.noreply.github.com>
2021-10-04 19:54:23 +0800

    Delete reinstall.sh

--- reinstall.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/sh
-sh <(curl -fsSL https://raw.githubusercontent.com/emptysuns/Hi_Hysteria/main/uninstall.sh)
-sh <(curl -fsSL https://raw.githubusercontent.com/emptysuns/Hi_Hysteria/main/install.sh)
-
