06bd9617ea2bf1f642cdf2ffe45afff1d4becd16
emptysuns <74051228+emptysuns@users.noreply.github.com>
2021-10-04 20:32:21 +0800

    Update reinstall.sh

--- server/reinstall.sh
+++ server/reinstall.sh
@@ -1,2 +1,7 @@
-sh <(curl -fsSL https://git.io/rmhysteria.sh)
-sh <(curl -fsSL https://git.io/hysteria.sh)
+#!/bin/sh
+wget -O /tmp/uninstall.sh https://git.io/rmhysteria.sh && chmod +x /tmp/uninstall.sh && sh /tmp/uninstall.sh
+echo "\033[41;37mUninstall Complete!\033[0m\n\n"
+echo "\033[41;37mStart reinstall!\033[0m\n"
+wget -O /tmp/install.sh https://git.io/hysteria.sh && chmod +x /tmp/install.sh && sh /tmp/install.sh
+rm -rf /tmp/uninstall.sh /tmp/install.sh
+echo "\033[41;37mReinstall Complete!\033[0m\n"
