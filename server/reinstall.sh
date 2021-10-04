8f323929dcb38a49053461d88d9ffd4a78df4e76
emptysuns <74051228+emptysuns@users.noreply.github.com>
2021-10-05 01:02:35 +0800

    Update reinstall.sh
    --no-check-certificate

--- server/reinstall.sh
+++ server/reinstall.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 echo "\033[42;37mStart Uninstall!\033[0m\n\n"
-wget -O /tmp/uninstall.sh https://git.io/rmhysteria.sh && chmod +x /tmp/uninstall.sh && sh /tmp/uninstall.sh
+wget -O /tmp/uninstall.sh --no-check-certificate https://git.io/rmhysteria.sh && chmod +x /tmp/uninstall.sh && sh /tmp/uninstall.sh
 echo "\033[42;37mUninstall Complete!\033[0m\n\n"
 echo "\033[42;37mStart reinstall!\033[0m\n"
-wget -O /tmp/install.sh https://git.io/hysteria.sh && chmod +x /tmp/install.sh && sh /tmp/install.sh
+wget -O /tmp/install.sh --no-check-certificate https://git.io/hysteria.sh && chmod +x /tmp/install.sh && sh /tmp/install.sh
 rm -rf /tmp/uninstall.sh /tmp/install.sh
 echo "\033[42;37mReinstall Complete!\033[0m\n"
