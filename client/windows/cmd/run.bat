fbabd89c3d764e8b9aac956151d82c20a59fd9c9
emptysuns <74051228+emptysuns@users.noreply.github.com>
2021-10-04 20:00:22 +0800

    Update run.bat

--- client/windows/cmd/run.bat
+++ client/windows/cmd/run.bat
@@ -14,9 +14,9 @@ if /i "%check%"=="Y" goto stop
 if /i "%check%"=="n" goto running else(goto stop)
 :stop
 echo Start Clear! 
-reg add "HKCU\Software\Microsoft\Windows\CurrentVersion\Internet Settings" /v ProxyEnable /t REG_DWORD /d 0 /f
-reg add "HKCU\Software\Microsoft\Windows\CurrentVersion\Internet Settings" /v ProxyServer /d "" /f
-reg add "HKCU\Software\Microsoft\Windows\CurrentVersion\Internet Settings" /v ProxyOverride /t REG_SZ /d "" /f
+REG add "HKCU\Software\Microsoft\Windows\CurrentVersion\Internet Settings" /v ProxyEnable /t REG_DWORD /d 0 /f
+REG add "HKCU\Software\Microsoft\Windows\CurrentVersion\Internet Settings" /v ProxyServer /d "" /f
+REG add "HKCU\Software\Microsoft\Windows\CurrentVersion\Internet Settings" /v ProxyOverride /t REG_SZ /d "" /f
 REG ADD "HKCU\SOFTWARE\Microsoft\Windows\CurrentVersion\Internet Settings" /v AutoDetect /t REG_DWORD /f /D 1
 for /f "tokens=5 delims= " %%i in ('netstat -ano ^| findstr "127.0.0.1:8888.*0.0.0.0"') do set PID=%%i
 taskkill /PID %PID% /F
