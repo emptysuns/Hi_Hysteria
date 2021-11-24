e07085c15242462527ce1ebbf71fcba9ca6e1986
emptysuns <74051228+emptysuns@users.noreply.github.com>
2021-11-24 14:23:47 +0800

    Update uninstall.sh

--- server/uninstall.sh
+++ server/uninstall.sh
@@ -5,7 +5,7 @@ rm -rf /etc/systemd/system/hysteria.service
 systemctl daemon-reload
 rm -rf /etc/hysteria/
 crontab -l > ./crontab.tmp
-sed -i '/0 \* \* \* \* systemctl restart hysteria/d' ./crontab.tmp
+sed -i '/0 4 \* \* \* systemctl restart hysteria/d' ./crontab.tmp
 crontab ./crontab.tmp
 rm -rf ./crontab.tmp
 echo "\033[42;37mUninstall complete!\033[0m\n"
