b5b073134b5e300eb28f7f9709e3109efac612ba
emptysuns <admin@imoeq.com>
2022-03-02 11:09:45 +0800

    fix del iptables rules

--- server/uninstall.sh
+++ server/uninstall.sh
@@ -8,10 +8,7 @@ crontab -l > ./crontab.tmp
 sed -i '/0 4 \* \* \* systemctl restart hysteria/d' ./crontab.tmp
 crontab ./crontab.tmp
 rm ./crontab.tmp
-iptables -L INPUT --line-numbers | grep hihysteria | awk '{print $1}' | while read line
-do
-iptables -D INPUT 1
-done
+iptables-save |  sed -e '/hihysteria/d' | iptables-restore
 netfilter-persistent save
 netfilter-persistent reload
 echo -e "\033[1;33;40mUninstall complete!\033[0m\n"
